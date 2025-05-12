32800a76d6eb77b2a9eabdc02bc85abd91968e5c
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-05-12 05:08:23 +0300

    Initial blog draft
    Adds a very basic implementation for blogging, only really useful for creating pages for now, and for Espe to work on CSS because I despise CSS

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,21 @@
 import { HtmlBasePlugin, InputPathToUrlTransformPlugin } from "@11ty/eleventy";
+import markdownIt from "markdown-it";
 import path from "node:path";
 import * as sass from "sass";
+import { readdirSync } from "node:fs";
 
 export default async function(eleventyConfig) {
 	const isDebug = process.argv.some(str => str.includes("serve") || str.includes("watch"));
+	const returnData = {
+		dir: {
+			input: "src",
+      		includes: "_includes",
+			data: "_data",
+			output: "_site"
+		},
+		markdownTemplateEngine: "njk",
+		htmlTemplateEngine: "njk",
+	};
 
 	// Data
 	eleventyConfig.addGlobalData("godsvg.version", "1.0-alpha8")
@@ -49,14 +61,44 @@ export default async function(eleventyConfig) {
 		},
 	});
 
-	return {
-		dir: {
-			input: "src",
-      		includes: "_includes",
-			data: "_data",
-			output: "_site"
-		},
-		markdownTemplateEngine: "njk",
-		htmlTemplateEngine: "njk",
+	// Blog things
+	eleventyConfig.addShortcode("blogimg", function(name, alt) {
+		/** @type {string[]} */
+		const e = this.page.url.split('/');
+		const slug = e[e.length-1];
+		return `![${alt}](/assets/blog/${slug}/${name})`
+	});
+	/** @type {markdownIt.Options} */
+	const mdOptions = {
+		html: true,
+		breaks: true,
+		linkify: true,
 	};
+	eleventyConfig.addTemplateFormats("md");
+	eleventyConfig.addExtension("md", {
+		outputFileExtension: "html",
+
+		/** @param {string} inputContent; @param {string} inputPath */
+		compile: async function(inputContent, inputPath) {
+			// FIXME: Skipping files does not currently work; it writes files with "null" or "undefined" as content instead.
+			//        https://www.11ty.dev/docs/languages/custom/#skipping-a-template-from-inside-of-the-compile-function
+			if (!inputPath.startsWith("./src/articles/")) {
+				return;
+			}
+
+			const md = markdownIt(mdOptions);
+			let rendered = md.render(inputContent)
+			return () => rendered;
+		}
+	});
+	const articlesPath = path.join(returnData.dir.input, "articles");
+	readdirSync(articlesPath)  // Moving over media files.
+		.filter(file => file != "articles.json")
+		.forEach(file => {
+			const article = path.join(articlesPath, file);
+			eleventyConfig.addPassthroughCopy({ [path.join(article, "media")]: `/assets/blog/${file}` });
+			eleventyConfig.addPassthroughCopy({ [path.join(article, "cover.webp")]: `assets/blog/${file}/cover.webp` });
+		});
+
+	return returnData;
 };
---
+++ src/articles/1.0-alpha1/article.md
@@ -0,0 +1,34 @@
+---
+slugcat: 1.0-alpha1
+title: GodSVG first development build is out!
+tags: release
+prerelease: true
+---
+
+A few months ago in May, I started working on an SVG editor inside Godot called GodSVG. In June I open-sourced it. After 5 months of hard work and with help from a few contributors, I've made GodSVG usable and have lifted all of the roadblocks to an initial alpha release. The last blocker was the lack of Undo and Redo, but now they have been implemented and I'm ready to present GodSVG to the world. 
+
+# A code-focused SVG editor
+
+{% blogimg "code_selection.webp", "Image of doing a code selection" %}
+
+GodSVG's main goal is to try to be the best SVG editor out there for working with the code and structure of SVG files directly. It represents the SVG hierarchy exactly as it is and allows you to edit the SVG's text and see changes in real time. This should make GodSVG great for creating simple SVGs and for optimizing, although this potential is not realized yet.
+
+# Other editing methods
+
+Aside from the code editor, there is an inspector which allows to edit some attributes with the help of various widgets. Color fields come with a color wheel widget, as well as palettes that can be adjusted in the settings. The path editor took especially long to get right: It explicitly represents the pathdata as a list of path commands, allowing you to edit the numbers to be precisely as you want.
+
+{% blogimg "path_element.webp", "Image of a path element" %}
+
+And of course, there is the viewport, which can be panned and zoomed, and it has handles for adjusting the geometry of paths and other shapes. It has a convenient adaptable grid, a checkerboard pattern, options for hiding the grid and the handles, and for viewing the rasterized SVG.
+
+# Downloads
+
+{Warning: This is by definition a pre-release piece of software. Be sure to back up your SVGs to preserve them in case of corruption.}
+
+[Widget for downloads]
+
+# Support
+
+Without donations, GodSVG would be way behind where it is now. If you're able to donate, I would be very grateful!
+
+[Buttons for donations or widget]
