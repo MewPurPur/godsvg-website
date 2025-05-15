ceb7efd6f2b2301356168f5cea4c12a5004c0db1
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2025-05-15 13:10:56 +0300

    Fix article downloads buttons showing the wrong platforms, and styling (#29)

--- src/_data/downloads.js
+++ src/_data/downloads.js
@@ -1,18 +1,29 @@
+import fs from "node:fs"
+import path from "node:path"
+
 // Release download link generator
 
 export default function(configData) {
     return function(version=null) {
         if (version == null) version = configData.godsvg.version;
-        if (version != "latest") {
-            version = "v" + version;
-        }
-        const base = `https://github.com/MewPurPur/GodSVG/releases/download/${version}`;
-        
-        return {
-            android: `${base}/GodSVG.Android.apk`,
+        const base = `https://github.com/MewPurPur/GodSVG/releases/download/${version != "latest" ? "v"+version : version}`;
+        const platformToDownload = {
+            android: `${base}/GodSVG.Android.zip`,
             linux:   `${base}/GodSVG.Linux.zip`,
             macos:   `${base}/GodSVG.MacOS.zip`,
             windows: `${base}/GodSVG.Windows.zip`
         }
+
+        // Reading release data
+        const releasesPath = path.join(configData.eleventy.directories.data, "releases.json")
+        const releasesFile = fs.readFileSync(releasesPath, { encoding: "utf-8" });
+        const releasesJson = JSON.parse(releasesFile)[version];
+
+        // Creating the object
+        const output = {}
+        releasesJson.platforms.forEach(platform => {
+            output[platform] = platformToDownload[platform];
+        });
+        return output;
     }
 }
