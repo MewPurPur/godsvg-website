62871ede8f08277779387328e400b4c49caf96c5
MewPurPur <mew.pur.pur@gmail.com>
2025-05-15 18:59:59 +0300

    Improve website

--- src/articles/articles.11tydata.js
+++ src/articles/articles.11tydata.js
@@ -1,6 +1,12 @@
 export default {
-    layout: "article.njk",
-    tags: "articles",
+	layout: "article.njk",
+	tags: "articles",
+	eleventyComputed: {
+		isRetroactive: data => {
+			let postTs = new Date(data.page.date).getTime();
+			return postTs < new Date("2025-05-10").getTime();
+		}
+	},
 	permalink: function ({ slugcat }) {
 		return `/article/${this.slugify(slugcat)}/index.html`;
 	},
