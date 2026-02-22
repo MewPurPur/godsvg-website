7dae520fb025918709ff20706d1c5b0c546b744a
MewPurPur <mew.pur.pur@gmail.com>
2026-02-22 16:18:06 +0200

    Fix alpha14 missing from releases

--- eleventy.config.js
+++ eleventy.config.js
@@ -21,7 +21,7 @@ export default async function(eleventyConfig) {
 	};
 
 	// Data
-	eleventyConfig.addGlobalData("godsvg.version", "1.0-alpha13")
+	eleventyConfig.addGlobalData("godsvg.version", "1.0-alpha14")
 	eleventyConfig.addGlobalData("site.url", isDebug ? "" : "https://godsvg.com")
 
 	// Plugins
